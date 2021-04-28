9e43be5fea5b4e40c383c4eadb1523d507a1f2d5
Kito <webdevkito@gmail.com>
2021-04-28 16:33:17 -0400

    iteration 3

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,18 +1,28 @@
-//Iteration 2
+// Iteration 3
+//------------
 
-//Must be <= 8 characters
+// Must also be able to accept admin users who have more strict specifications
+//--------------------------------------------------------------------------
 
-//Must contain 1 letter
+// REGULAR
+// Must be <= 8 characters
+// Must contain at least 1 letter
+// Must contain at least 1 number
+// Must also indicate why password is not valid
+//------------------------------------------------
 
-//Must contain 1 number
-
-//Must also indicate why password is not valid
-
+// ADMIN
+// Must be <= 13 characters
+// Must contain at least 1 special character
+// Must contain at least 1 letter
+// Must contain at least 1 number
+// Must also indicate why password is not valid
+//------------------------------------------
 
 
 class PasswordChecker{
     constructor() {}
-    validatePassword(password){
+    validateStandardPassword(password){
         let valid = true;
         let message = [];
         if(password.length < 8){
@@ -37,8 +47,46 @@ class PasswordChecker{
             return 'Password NOT valid' + message.join('')
         }
     }
-}
 
+    validateAdminPassword(password){
+        let valid = true;
+        let message = [];
+        if(password.length < 13){
+            message.push('\nMust be at least 13 characters')
+            valid = false;
+        }
+        if(/[a-z]/.exec(password) == null){
+            message.push('\nMust contain at least 1 letter')
+            valid = false;
+        }
+        if(/[0-9]/.exec(password) == null){
+            message.push('\nMust contain at least 1 number')
+            valid = false;
+        }
+        if(/[!@#$%^&*]/.exec(password) == null){
+            message.push('\nMust contain at least 1 special character')
+            valid = false;
+        }
+        else{
+            //pass
+        }
+        if(valid == true){
+            return 'Password VALID'
+        }
+        else{
+            return 'Password NOT valid' + message.join('')
+        }
+    }
+
+    RunCheck(password, user_type){
+        if(user_type == 'admin'){
+           return this.validateAdminPassword(password);
+        }
+        else{
+            return this.validateStandardPassword(password)
+        }
+    }
+}
 try{
     module.exports = {PasswordChecker}
 }
--- test/test_passwordChecker.js
+++ test/test_passwordChecker.js
@@ -3,19 +3,39 @@ const {PasswordChecker} = require('../src/passwordChecker')
 
 let testChecker = new PasswordChecker()
 
-describe("Test 'PasswordChecker' class", () => {
+describe("Test PasswordChecker class", () => {
+    describe("Admin test", () => {
+        it("Should return invalid and inform user of requirements if password is null", () => { assert.strictEqual(testChecker.RunCheck('', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 letter\nMust contain at least 1 number\nMust contain at least 1 special character') })
 
-    it("Should return invalid and inform user of requirements if password is null", () => { assert.strictEqual(testChecker.validatePassword(''), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter, at least 1 number, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('!', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 letter\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter, at least 1 special character, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('0', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 letter\nMust contain at least 1 special character') })
+        it("Should return invalid and inform user if password does not contain, at least 1 number, at least 1 special character, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('p', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 number\nMust contain at least 1 special character') })
 
+        it("Should return invalid and inform user if password does not contain, at least 1 letter, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('1!', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 letter') })
+        it("Should return invalid and inform user if password does not contain, at least 1 special character, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('p3', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 special character') })
+        it("Should return invalid and inform user if password does not contain, at least 1 number, and is not at least 13 characters.", () => { assert.strictEqual(testChecker.RunCheck('!p', 'admin'), 'Password NOT valid\nMust be at least 13 characters\nMust contain at least 1 number') })
 
-    it("Should return invalid and inform user if password does not contain at least 1 number and is not at least 8 characters.", () => { assert.strictEqual(testChecker.validatePassword('passwor'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 number') })
-    it("Should return invalid and inform user if password does not contain at least 1 letter and is not at least 8 characters.", () => { assert.strictEqual(testChecker.validatePassword('1234567'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter') })
-    it("Should return invalid and inform user if password does not contain at least 1 letter and does not contain at least 1 number", () => { assert.strictEqual(testChecker.validatePassword('!@#$%^&*'), 'Password NOT valid\nMust contain at least 1 letter\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain at least 1 letter and at least 1 special character", () => { assert.strictEqual(testChecker.RunCheck('12345678901234', 'admin'), 'Password NOT valid\nMust contain at least 1 letter\nMust contain at least 1 special character') })
+        it("Should return invalid and inform user if password does not contain, at least 1 special character and at least 1 number", () => { assert.strictEqual(testChecker.RunCheck('passwordpassword', 'admin'), 'Password NOT valid\nMust contain at least 1 number\nMust contain at least 1 special character') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter and at least 1 number", () => { assert.strictEqual(testChecker.RunCheck('!@#$%^&*!@#$%', 'admin'), 'Password NOT valid\nMust contain at least 1 letter\nMust contain at least 1 number') })
 
+        it("Should return invalid and inform user if password does not contain, at least 1 special character", () => { assert.strictEqual(testChecker.RunCheck('password1234567', 'admin'), 'Password NOT valid\nMust contain at least 1 special character') })
+        it("Should return invalid and inform user if password does not contain, at least 1 number", () => { assert.strictEqual(testChecker.RunCheck('password!@#$%', 'admin'), 'Password NOT valid\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter", () => { assert.strictEqual(testChecker.RunCheck('123456!@#$%^&*', 'admin'), 'Password NOT valid\nMust contain at least 1 letter') })
+        
 
-    it("Should return invalid and inform user if password does not contain at least 1 letter.", () => { assert.strictEqual(testChecker.validatePassword('12345678'), 'Password NOT valid\nMust contain at least 1 letter') })
-    it("Should return invalid and inform user if password does not contain at least 1 number", () => { assert.strictEqual(testChecker.validatePassword('password'), 'Password NOT valid\nMust contain at least 1 number') })
-    it("Should return invalid invalid and inform user if password is too short", () => { assert.strictEqual(testChecker.validatePassword('pass123'), 'Password NOT valid\nMust be at least 8 characters') })
+        it("Should return VALID if the password meets the requirements", () => { assert.strictEqual(testChecker.RunCheck('password1234!', 'admin'), 'Password VALID') })
+    })
+    describe("Standard/Normal test", () => {
+        it("Should return invalid and inform user of requirements if password is null", () => { assert.strictEqual(testChecker.RunCheck('', 'normal'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter\nMust contain at least 1 number') })
 
-    it("Should return valid if password meets requirements", () => { assert.strictEqual(testChecker.validatePassword('password123'), 'Password VALID') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter, at least 1 number, and is not at least 8 characters.", () => { assert.strictEqual(testChecker.RunCheck('!', 'normal'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain, at least 1 letter, and is not at least 8 characters.", () => { assert.strictEqual(testChecker.RunCheck('0', 'normal'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter') })
+
+        it("Should return invalid and inform user if password is not at least 8 characters.", () => { assert.strictEqual(testChecker.RunCheck('pass12', 'normal'), 'Password NOT valid\nMust be at least 8 characters') })
+        it("Should return invalid and inform user if password does not contain at least 1 number", () => { assert.strictEqual(testChecker.RunCheck('password', 'normal'), 'Password NOT valid\nMust contain at least 1 number') })
+        it("Should return invalid and inform user if password does not contain at least 1 letter", () => { assert.strictEqual(testChecker.RunCheck('12345678', 'normal'), 'Password NOT valid\nMust contain at least 1 letter') })
+
+        it("Should return VALID if the password meets the requirements", () => { assert.strictEqual(testChecker.RunCheck('password1234', 'normal'), 'Password VALID') })
+    })
 })
