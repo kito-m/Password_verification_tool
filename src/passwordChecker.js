2e1676ff2180a4612ffa6aa1d55035dd99ec390b
Kito <webdevkito@gmail.com>
2021-04-28 16:28:38 -0400

    iteration 2

--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,37 +1,42 @@
-// Iteration 1
+//Iteration 2
 
-// Must be <= 8 characters
+//Must be <= 8 characters
 
-// Must contain 1 letter
+//Must contain 1 letter
 
-// Must contain 1 number
+//Must contain 1 number
+
+//Must also indicate why password is not valid
+
+
 
 class PasswordChecker{
+    constructor() {}
     validatePassword(password){
-        let flag = 0;
-        let message = "";
-        while(true){
-            if(password.length < 8){
-                flag = -1;
-                break;
-            }
-            else if(/[a-z]/.exec(password) == null){
-                flag = -1;
-                break;
-            }
-            else if(/[0-9]/.exec(password) == null){
-                flag = -1;
-                break;
-            }
-            else{
-                return 'Valid password';
-            }
+        let valid = true;
+        let message = [];
+        if(password.length < 8){
+            message.push('\nMust be at least 8 characters')
+            valid = false;
         }
-        if(flag == -1){
-            return 'Password not valid'
+        if(/[a-z]/.exec(password) == null){
+            message.push('\nMust contain at least 1 letter')
+            valid = false;
+        }
+        if(/[0-9]/.exec(password) == null){
+            message.push('\nMust contain at least 1 number')
+            valid = false;
+        }
+        else{
+            //pass
+        }
+        if(valid == true){
+            return 'Password VALID'
+        }
+        else{
+            return 'Password NOT valid' + message.join('')
         }
     }
-
 }
 
 try{
--- test/test_passwordChecker.js
+++ test/test_passwordChecker.js
@@ -3,9 +3,19 @@ const {PasswordChecker} = require('../src/passwordChecker')
 
 let testChecker = new PasswordChecker()
 
-describe("Test PasswordChecker class", () => {
-    it("Should return invalid if password is less than 8 characters.", () => {assert.strictEqual(testChecker.validatePassword('pass1'), 'Password not valid')})
-    it("Should return invalid if password does not contain at least 1 letter", () => {assert.strictEqual(testChecker.validatePassword('12345678'), 'Password not valid')})
-    it("Should return invalid if password does not contain at least 1 number", () => {assert.strictEqual(testChecker.validatePassword('password'), 'Password not valid')})
-    it("Should return valid if password meets requirements", () => {assert.strictEqual(testChecker.validatePassword('password123'), 'Valid password')})
+describe("Test 'PasswordChecker' class", () => {
+
+    it("Should return invalid and inform user of requirements if password is null", () => { assert.strictEqual(testChecker.validatePassword(''), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter\nMust contain at least 1 number') })
+
+
+    it("Should return invalid and inform user if password does not contain at least 1 number and is not at least 8 characters.", () => { assert.strictEqual(testChecker.validatePassword('passwor'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 number') })
+    it("Should return invalid and inform user if password does not contain at least 1 letter and is not at least 8 characters.", () => { assert.strictEqual(testChecker.validatePassword('1234567'), 'Password NOT valid\nMust be at least 8 characters\nMust contain at least 1 letter') })
+    it("Should return invalid and inform user if password does not contain at least 1 letter and does not contain at least 1 number", () => { assert.strictEqual(testChecker.validatePassword('!@#$%^&*'), 'Password NOT valid\nMust contain at least 1 letter\nMust contain at least 1 number') })
+
+
+    it("Should return invalid and inform user if password does not contain at least 1 letter.", () => { assert.strictEqual(testChecker.validatePassword('12345678'), 'Password NOT valid\nMust contain at least 1 letter') })
+    it("Should return invalid and inform user if password does not contain at least 1 number", () => { assert.strictEqual(testChecker.validatePassword('password'), 'Password NOT valid\nMust contain at least 1 number') })
+    it("Should return invalid invalid and inform user if password is too short", () => { assert.strictEqual(testChecker.validatePassword('pass123'), 'Password NOT valid\nMust be at least 8 characters') })
+
+    it("Should return valid if password meets requirements", () => { assert.strictEqual(testChecker.validatePassword('password123'), 'Password VALID') })
 })
